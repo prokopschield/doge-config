d9ee1e2b93cce6ba9e55ccd06a4243623975a977
Prokop <prokop.schield@protonmail.com>
2021-07-31 20:03:19 +0200

    0.3.8 add `getConfigDir`

---
+++ src/utils/getConfigDir.ts
@@ -0,0 +1,23 @@
+import { ConfigDir } from '../classes/ConfigDir';
+import normalizeConfigName from './normalizeConfigName';
+
+const configs = new Map<string, ConfigDir>();
+
+export function getConfigDir(name: string, defaults?: object): ConfigDir {
+	name = normalizeConfigName(name);
+	const config = configs.get(name);
+	if (config) {
+		return config;
+	} else {
+		const config = new ConfigDir(name, defaults);
+		configs.set(name, config);
+		return config;
+	}
+}
+
+export default module.exports = getConfigDir;
+
+Object.defineProperties(getConfigDir, {
+	default: { get: () => getConfigDir },
+	getConfigDir: { get: () => getConfigDir },
+});
